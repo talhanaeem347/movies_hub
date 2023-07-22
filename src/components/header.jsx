934fbc56e45cf2cca355fd2b20fc903b9f0959a7
Muhammad Talha <talhanaeem347@gmail.com>
2023-07-22 22:12:27 +0500

    header ui is complete

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,4 +1,5 @@
-
+import AddIcon from '@mui/icons-material/Add';
+import Button from '@mui/material/Button';
 const Header = () => {
     return (
         <header className="w-full border-b-2">
@@ -7,7 +8,8 @@ const Header = () => {
                     <span className="text-red-500">Movies</span>
                     Hub
                 </h1>
-                <button className="bg-green-500 px-4     border-2 border-white rounded-md hover:bg-green-600 focus:bg-green-400 ">add New</button>
+                {/* <button className="bg-green-500 px-2  border-2 border-white rounded-md hover:bg-green-600 focus:bg-green-400 "><AddIcon/> add New</button> */}
+                <Button variant="contained" color='success' className=" border-2 border-white rounded-md hover:bg-green-600 focus:bg-green-400 " ><AddIcon/> add New</Button>
             </div>
         </header>
     );
